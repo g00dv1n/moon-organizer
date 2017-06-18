b7934411d9b94c60bd9f19c0145e87fc44f7f5fd
Alexandr Brik <g00dv1n.private@gmail.com>
2017-06-19 00:04:17 +0300

    birthday

---
+++ src/lunar-birthday/1.js
@@ -0,0 +1,67 @@
+const symbol = {
+  ru: '',
+  en: 'Candle, Lamp, Third eye'
+}
+
+const energetics = {
+  ru: '',
+  en: 'active'
+}
+
+const keywords = {
+  ru: '',
+  en: 'analysis, compromise, peace, planning, thinking'
+}
+
+const element = {
+  ru: '',
+  en: 'Water'
+}
+
+const direction = {
+  ru: '',
+  en: 'North'
+}
+
+const shape = {
+  ru: '',
+  en: 'wavy lines, asymmetry'
+}
+
+const stone = {
+  ru: '',
+  en: 'diamond, rhinestone'
+}
+
+const color = {
+  ru: '',
+  en: 'red, scarlet, black'
+}
+
+const p1 = {
+  ru: '',
+  en: 'The people who were born on the first lunar day are dreamers and stay children for a lifetime, waiting for a miracle. They have strong thinking power, and if think positively, they\'re very lucky. <b>Be careful about what and how you think.</b>'
+}
+
+const p2 = {
+  ru: '',
+  en: 'Born on the first lunar day can reach the sky - in the condition that they will start to do something. For many "new moon" children it is hard to embark to the realization of the conceived plans, it is very tempting for them to spend all their life in oblivion, in anticipation of something, in dreams of deeds, glory, great accomplishments. However, it is worth overcoming and taking the first step, as the plans of those who born on the 1st lunar day are realized quite easily and quickly to the surprise of others. <b>Just start to do something from your dream list!</b>'
+}
+
+const p3 = {
+  ru: '',
+  en: 'The second trap, after the temptation to dissolve into dreams, is to work on wear and tear. Born on the first lunar day find it hard to restore strength, the guarantee of health and longevity for them is in the thorough care of yourself. <b>Do not overwork, and you\'ll keep energy for a long time.</b>'
+}
+
+const paragraphs = [p1, p2, p3]
+export default {
+  symbol,
+  energetics,
+  keywords,
+  element,
+  direction,
+  shape,
+  stone,
+  color,
+  paragraphs
+}
